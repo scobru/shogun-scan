45c9db5597416e421f92ffaec0994c858d8d4edd
unknown <scobru1988@gmail.com>
2025-03-27 08:52:02 +0100

    Convertito shogun-core da directory a submodule

--- shogun-protocol/lib/messaging/index.js
+++
@@ -1,8 +0,0 @@
-import chatsList from "./chatsList";
-import createChat from "./createChat";
-import messageList from "./messageList";
-import sendMessage from "./sendMessage";
-import sendVoiceMessage from "./sendVoiceMessage";
-
-export { createChat, chatsList, messageList, sendMessage, sendVoiceMessage };
-
--- shogun-wallet-app/src/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-export const log = (message: string, ...args: any[]) => {
-  console.log(`[Shogun] ${message}`, ...args);
-};
-
-export const logError = (message: string, ...args: any[]) => {
-  console.error(`[Shogun] ${message}`, ...args);
-}; 
